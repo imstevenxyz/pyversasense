4828b95b76a82bc6014380bb4981f980aec6edce
Steven Impens <s.impens.dev@gmail.com>
2019-04-08 14:28:53 +0200

    creation of setup and test method

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyversasense',
+      version='0.1',
+      description='Versasense API consumer',
+      url='',
+      author='Steven Impens',
+      author_email='s.impens.dev@gmail.com',
+      license='MIT',
+      packages=['pyversasense'],
+      zip_safe=False)
