2c2a12e058bcebb03c6fada53fb7e4ea4d66ca47
Steven Impens <s.impens.dev@gmail.com>
2019-04-09 23:28:44 +0200

    Convert code to use asyncio and aiohttp

--- pyversasense/__init__.py
+++ pyversasense/__init__.py
@@ -1,13 +1,22 @@
+import asyncio
+import aiohttp
+
 from .consumer import Consumer
 
-def test():
-    url = "https://107731e1-6ad3-4fac-937c-e115c6b5343f.mock.pstmn.io"
-    #url = "https://gateway.versasense.com:8889"
-    consumer = Consumer(url)
-    consumer.fetchDevices()
-    for d in consumer.deviceList:
-        for per in d.peripherals:
-            print(per.identifier)
-            print(consumer.fetchPeripheralSample(per))
+url = "https://107731e1-6ad3-4fac-937c-e115c6b5343f.mock.pstmn.io"
+#url = "https://gateway.versasense.com:8889"
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        consumer = Consumer(url, session)
+        await consumer.fetchDevices()
+        for d in consumer.deviceList:
+            for per in d.peripherals:
+                print(per.identifier)
+                sample = await consumer.fetchPeripheralSample(per)
+                print(sample)
 
     return True
+
+def test():
+    asyncio.get_event_loop().run_until_complete(main())
--- pyversasense/consumer.py
+++ pyversasense/consumer.py
@@ -1,18 +1,20 @@
 import json
-import requests
+import asyncio
+import aiohttp
 
 from .device import Device
 from .peripheral import Peripheral
 from .const import (ENDPOINT_DEVICES)
 
-header = {'Content-Type': 'application/json'}
+headers = {'Content-Type': 'application/json'}
 
 class Consumer:
 
     deviceList = []
 
-    def __init__(self, host):
+    def __init__(self, host, webSession):
         self._host = host
+        self._webSession = webSession
 
     @property
     def host(self):
@@ -22,53 +24,73 @@ class Consumer:
     def host(self, host):
         self._host = host
 
-    def fetchPeripheralSample(self, peripheral):
-        url = self._host + ENDPOINT_DEVICES + "/" + peripheral.parentMac + "/peripherals/" + peripheral.identifier + "/sample"
-        response = requests.get(url, header)
-        print(response)
-        return response.json()
+    @property
+    def webSession(self):
+        return self._webSession
+    
+    @webSession.setter
+    def webSession(self, webSession):
+        self._webSession = webSession
 
-    def fetchDevices(self):
+    async def fetchPeripheralSample(self, peripheral):
+        url = self._host + ENDPOINT_DEVICES + "/" + peripheral.parentMac + "/peripherals/" + peripheral.identifier + "/sample"
+        response = "e"
+        try:
+            response = await self._webRequest(self._webSession, url)
+        except Exception as e:
+            print(e)
+        return response
+
+    async def fetchDevices(self):
         self.deviceList.clear()
         url = self._host + ENDPOINT_DEVICES
         try:
-            response = requests.get(url, header)
+            response = await self._webRequest(self._webSession, url)
         except Exception as e:
             print(e)
             return False
 
-        if response.status_code == 200:
-            self._jsonToDeviceList(response.json())
-            return True
+        print(response)
 
-        return False
+        self.deviceList =  _jsonToDeviceList(response)
 
-    def _jsonToDeviceList(self, json):
-        for jsonDevice in json:
-            address = jsonDevice["address"]
-            peripherals = jsonDevice["peripherals"]
-            name = jsonDevice["name"]
-            description = jsonDevice["description"]
-            location = jsonDevice["location"]
-            type = jsonDevice["type"]
-            battery = jsonDevice["battery"]
-            version =jsonDevice["version"]
-            mac = jsonDevice["mac"]
-            status = jsonDevice["status"]
-            
-            peripheralList = self._jsonToPeripheralList(peripherals, mac)
+        return True
 
-            self.deviceList.append(Device(address, peripheralList, name, description, location, type, battery, version, mac, status))
+    async def _webRequest(self, websession, url):
+        async with websession.get(url, headers=headers) as response:
+            print(response.status)
+            if response.status == 200:
+                data = await response.json(content_type=None)
+            else:
+                raise Exception('Bad response status code: {}'.format(response.status))
+        return data
 
-    def _jsonToPeripheralList(self, json, parentMac):
-        peripheralList =  []
-        for peripheral in json:
-            samplingRate = peripheral["sampling_rate"]
-            identifier = peripheral["identifier"]
-            lastUpdated = peripheral["last_updated"]
-            color = peripheral["color"]
-            icon = peripheral["icon"]
-            text = peripheral["text"]
-            classification = peripheral["class"]
-            peripheralList.append(Peripheral(samplingRate, identifier, lastUpdated, color, icon, text, classification, parentMac))
-        return peripheralList
+def _jsonToDeviceList(json):
+    deviceList = []
+    for jsonDevice in json:
+        address = jsonDevice["address"]
+        peripherals = jsonDevice["peripherals"]
+        name = jsonDevice["name"]
+        description = jsonDevice["description"]
+        location = jsonDevice["location"]
+        type = jsonDevice["type"]
+        battery = jsonDevice["battery"]
+        version =jsonDevice["version"]
+        mac = jsonDevice["mac"]
+        status = jsonDevice["status"]
+        peripheralList = _jsonToPeripheralList(peripherals, mac)
+        deviceList.append(Device(address, peripheralList, name, description, location, type, battery, version, mac, status))
+    return deviceList
+
+def _jsonToPeripheralList(json, parentMac):
+    peripheralList =  []
+    for peripheral in json:
+        samplingRate = peripheral["sampling_rate"]
+        identifier = peripheral["identifier"]
+        lastUpdated = peripheral["last_updated"]
+        color = peripheral["color"]
+        icon = peripheral["icon"]
+        text = peripheral["text"]
+        classification = peripheral["class"]
+        peripheralList.append(Peripheral(samplingRate, identifier, lastUpdated, color, icon, text, classification, parentMac))
+    return peripheralList
