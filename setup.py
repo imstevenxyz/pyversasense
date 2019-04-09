15dcd4e92c55d0b096c89fb86b02e65c39b96429
Steven Impens <s.impens.dev@gmail.com>
2019-04-10 00:06:50 +0200

    Edits for requirements en setup

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup
 
-setup(name='pyversasense',
-      version='0.1',
-      description='Versasense API consumer',
-      url='',
-      author='Steven Impens',
-      author_email='s.impens.dev@gmail.com',
-      license='MIT',
-      packages=['pyversasense'],
-      zip_safe=False)
+long_description = open('README.md').read()
+with open('requirements.txt') as req:
+    requirements = req.read().splitlines()
+
+setup(
+    name='pyversasense-flamm3blemuff1n',
+    version='0.0.1',
+    description='Versasense API consumer',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/flamm3blemuff1n/pyversasense',
+    author='Steven Impens',
+    author_email='s.impens.dev@gmail.com',
+    license='Apache License 2.0',
+    packages=['pyversasense'],
+    zip_safe=True,
+    install_requires=requirements
+)
