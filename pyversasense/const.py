af16dbc28fef25670bb274b124da5efbf240797e
Steven Impens <s.impens.dev@gmail.com>
2019-05-20 21:06:41 +0200

    Added endpoints for sample and actuation

--- pyversasense/const.py
+++ pyversasense/const.py
@@ -1,1 +1,5 @@
 ENDPOINT_DEVICES = "/api/v1/devices"
+ENDPOINT_SAMPLE = "/api/v1/devices/{}/peripherals/{}/sample"
+ENDPOINT_ACTUATE = "/api/v1/devices/{}/peripherals/{}/actuate"
+
+HEADER = {'Content-Type': 'application/json'}
