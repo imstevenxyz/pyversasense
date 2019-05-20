3e027e9c37829e21f0af6d3eb042a3cfd6191a6e
Steven Impens <s.impens.dev@gmail.com>
2019-05-20 21:35:52 +0200

    Changed version and added minor fixes

--- pyversasense/consumer.py
+++ pyversasense/consumer.py
@@ -3,7 +3,7 @@ import asyncio
 import aiohttp
 
 from .jsonHelpers import jsonToSampleList, jsonToDeviceDict
-from .const import (ENDPOINT_DEVICES, ENDPOINT_SAMPLE, HEADER)
+from .const import (ENDPOINT_DEVICES, ENDPOINT_SAMPLE, ENDPOINT_ACTUATE, HEADER)
 
 class Consumer:
 
@@ -48,18 +48,18 @@ class Consumer:
         samples = jsonToSampleList(response)
         return samples
 
-    async def actuatePeripheral(self, peripheral=None, identifier=None, parentMac=None, data=None):
+    async def actuatePeripheral(self, peripheral=None, identifier=None, parentMac=None, payload=None):
         """Actuate a peripheral."""
         url =  self._host
         if peripheral is not None:
-            url += ENDPOINT_SAMPLE.format(peripheral.parentMac, peripheral.identifier)
+            url += ENDPOINT_ACTUATE.format(peripheral.parentMac, peripheral.identifier)
         elif identifier is not None and parentMac is not None:
-            url += ENDPOINT_SAMPLE.format(parentMac, identifier)
+            url += ENDPOINT_ACTUATE.format(parentMac, identifier)
         else:
             raise ValueError("Bad arguments")
 
         try:
-            response = await _putRequest(self._webSession, url, data)
+            response = await _putRequest(self._webSession, url, payload)
         except Exception as e:
             print(e)
             return None
@@ -88,8 +88,9 @@ async def _getRequest(websession, url):
             raise Exception('Bad response status code: {}'.format(response.status))
     return data
 
-async def _putRequest(websession, url, data):
-    async with websession.put(url, headers=HEADER, data=data) as response:
+async def _putRequest(websession, url, payload):
+    print(url)
+    async with websession.put(url, headers=HEADER, json=payload) as response:
         if response.status == 200:
             data = await response.json(content_type=None)
         else:
--- tests/test.py
+++ tests/test.py
@@ -3,11 +3,11 @@ import aiohttp
 import asyncio
 from pyversasense import Consumer
 
-url = "https://107731e1-6ad3-4fac-937c-e115c6b5343f.mock.pstmn.io"
+url = "http://localhost:3000"
 deviceMacs = ["00-17-0D-00-00-30-E9-A7", "00-17-0D-00-00-30-DC-5E", "00-17-0D-00-00-30-E9-62", "00-17-0D-00-00-30-DB-2B", "00-17-0D-00-00-58-BD-01"]
-peripheralIds = ["8040/8042", "9803/9805", "1010/9000", "3303/5702"]
-measurementNames = ["Temperature","Humidity","Light","Pressure","Battery capacity","Battery used","Battery level"]
-samples = [41.18, 1765, 3000, 24.72, 43.45, 106.46, 1009.26]
+peripheralIds = ["8040/8042", "9803/9805", "1010/9000", "3303/5702", "3338/5850", "3302/5500"]
+measurementNames = ["Temperature","Humidity","Light","Pressure","Battery capacity","Battery used","Battery level",'Buzzer','Motion','Motion Numeric']
+samples = [41.18, 1765, 3000, 24.72, 43.45, 106.46, 1009.26, "OFF", "DETECTED", 1]
 
 class TestConsummer(asynctest.TestCase):
     async def setUp(self):
@@ -55,6 +55,15 @@ class TestConsummer(asynctest.TestCase):
             testcontrolesample = await self.consumer.fetchPeripheralSample(None, "3303/5702", "00-17-0D-00-00-30-E9-62")
             for sample in testcontrolesample:
                 self.assertIn(sample.value, samples)
+
+    async def test_peripheral_actuation(self):
+        async with aiohttp.ClientSession() as session:
+            self.consumer = Consumer(url, session)
+            self.deviceList = await self.consumer.fetchDevices()
+            
+            data = { "id": "state-num", "value": 1}
+            response = await self.consumer.actuatePeripheral(None, "3338/5850", "00-17-0D-00-00-30-E9-62", data)
+            print(response)
 
 if __name__ == '__main__':
     asynctest.main()
